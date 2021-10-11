6509e0206222e535c3c40ac9a5708ef99c458c44
T RAVI KUMAR <91825694+ravikumar990@users.noreply.github.com>
2021-10-11 09:14:58 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -12,5 +12,22 @@ public class TicTacToe {
      }
 
    }
+	
+	//Player choose
+	public static void playerChosse{
+		// declare variable
+		int playerX = 0;
+		//generate random number for player choose 
+		int player = (int) Math.floor(Math.random()* 10) % 2;
+		// check player
+		if (player == playerX) {
+			System.out.println("Player is x" );
+		}
+		else {
+			System.out.println("Player is O")
+			}
+	
+	
+	
  
 }
